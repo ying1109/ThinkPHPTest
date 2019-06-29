b9710c4784eff25f6e1e5b5898961f06349053c1
ying1109 <44599811+ying1109@users.noreply.github.com>
2019-06-29 17:06:39 +0800

    定时禁用按钮

---
+++ Application/Runtime/Cache/Admin/011c9f02fe0ac206e9b9ca5df7fed57b.php
@@ -0,0 +1,45 @@
+<?php if (!defined('THINK_PATH')) exit();?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="/Public/Admin/css/bootstrap.css">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="form-group">
+    <div class="col-sm-1 col-sm-offset-1">
+        <button type="submit" id="confirmReceipt" class="btn btn-success">提交</button>
+    </div>
+</div>
+
+</body>
+
+<script type="text/javascript" src="/Public/Admin/js/jquery-3.3.1.js"></script>
+<script type="text/javascript" src="/Public/Admin/js/bootstrap.js"></script>
+<script type="text/javascript">
+    $(function(){
+        $("#confirmReceipt").click(function () {
+            var count = 10;
+            var cound_down = setInterval(countDown, 1000);
+            $("#confirmReceipt").attr("disabled", true);
+            $("#confirmReceipt").text(count + " 秒后提交！");
+            console.log(111);
+            function countDown() {
+                count--;
+                console.log(count);
+                $("#confirmReceipt").attr("disabled", true);
+                $("#confirmReceipt").text(count + " 秒后提交！");
+                if (count == 0) {
+                    $("#confirmReceipt").text('提交').removeAttr('disabled');
+                    clearInterval(cound_down);
+                }
+            }
+//            var cound_down = setInterval(countDown, 1000);
+        })
+    });
+</script>
+
+</html>
